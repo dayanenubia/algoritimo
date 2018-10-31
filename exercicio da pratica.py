ffec43327fa9f471b2e9dacdd7d942524c137948
dayanenubia <dayane.nubia67@gmail.com>
2018-10-31 13:16:40 -0200

    Signed-off-by: dayanenubia <dayane.nubia67@gmail.com>

--- exercicio da pratica.py
+++ exercicio da pratica.py
@@ -7,17 +7,48 @@ def menu_de_opçoes ():
 
 def menu_de_opçoes_de_submenus ():
     print ("Menu de Opções de Salas")
-    print ("1. Incluir")
-    print ("2. Alterar")
-    print ("3. Imprimir")
-    print ("4. Excluir")
-    print ("5. Exibir")
+    print ("    1. Incluir")
+    print ("    2. Alterar")
+    print ("    3. Imprimir")
+    print ("    4. Excluir")
+    print ("    5. Exibir")
+   
 
 def submenu_de_salas ():
     opçao_de_submenu = int(input("Digite o número da opção desejada: "))
     if opçao_de_submenu == 1:
-        salas = {'codigo': ['nome' 'capacidade' 'tipo_de_exibição' 'acessivel']}
-        nome = 
+        a = {}
+        aux = {}
+        codigo = int(input("Digite o codigo: "))
+        nome = input("Digite o nome: ")
+        tipo_de_exebiçao = input("Digite o tipo de exibição: ")
+        acessivel = input("É acessível?: ")
+        aux ['Nome'] = nome
+        aux ['tipo de exebiçao'] = tipo_de_exebiçao
+        aux ['acessivel'] = acessivel
+        a [codigo] = aux
+    return a
+    if opçao_de_submenu == 2:
+        a = {}
+        aux = {}
+        codigo = int(input("Digite o codigo: "))
+        nome = input("Digite o nome: ")
+        tipo_de_exebiçao = input("Digite o tipo de exibição: ")
+        acessivel = input("É acessível?: ")
+        aux ['Nome'] = nome
+        aux ['tipo de exebiçao'] = tipo_de_exebiçao
+        aux ['acessivel'] = acessivel
+        a [codigo] = aux
+    return a
+        
+        
+        
+            
+        
+        
+        
+        
+        
 
 
 def opçoes ():
@@ -25,6 +56,8 @@ def opçoes ():
     if opçao_de_menu == 1:
         print ("Submenu de salas")
         menu_de_opçoes_de_submenus ()
+        sala = submenu_de_salas ()
+        print (sala)
     if opçao_de_menu == 2:
         submenu_de_filmes ()
         print ("Submenu de filmes")
